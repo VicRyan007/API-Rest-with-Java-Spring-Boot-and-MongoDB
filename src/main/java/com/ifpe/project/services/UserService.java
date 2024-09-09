7a9fdd2d679e74ff3e9af7b4e5378047ece4a03f
VicRyan007 <ryannautico306@gmail.com>
2024-09-08 23:44:00 -0300

    Add exceptions

--- src/main/java/com/ifpe/project/services/UserService.java
+++ src/main/java/com/ifpe/project/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.ifpe.project.domain.User;
 import com.ifpe.project.repository.UserRepository;
+import com.ifpe.project.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,6 +17,14 @@ public class UserService {
 
 	public List<User> findAll(){
 		return repo.findAll();
+	}
+	
+	public User findById(String id) {
+		User user = repo.findOne(id);
+		if (user == null) {
+			throw new ObjectNotFoundException("Objeto não encontrado");
+		}
+		return user;
 	}
 	
 }
