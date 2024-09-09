4f6c555f7ceea90452bfcc1f681b8e1845cdccc5
VicRyan007 <ryannautico306@gmail.com>
2024-09-08 22:29:06 -0300

    Add UserDTO

--- src/main/java/com/ifpe/project/resources/UserResource.java
+++ src/main/java/com/ifpe/project/resources/UserResource.java
@@ -1,15 +1,18 @@
 	package com.ifpe.project.resources;
 	
 	import java.util.List;
-	
-	import org.springframework.beans.factory.annotation.Autowired;
-	import org.springframework.http.ResponseEntity;
-	import org.springframework.web.bind.annotation.RequestMapping;
-	import org.springframework.web.bind.annotation.RequestMethod;
-	import org.springframework.web.bind.annotation.RestController;
-	
-	import com.ifpe.project.domain.User;
-	import com.ifpe.project.services.UserService;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ifpe.project.domain.User;
+import com.ifpe.project.dto.UserDTO;
+import com.ifpe.project.services.UserService;
 	
 	@RestController
 	@RequestMapping(value = "/users")
@@ -19,9 +22,10 @@
 		private UserService service;
 	
 		@RequestMapping(method=RequestMethod.GET)
-		public ResponseEntity<List<User>> findAll(){
+		public ResponseEntity<List<UserDTO>> findAll(){
 			List<User> list = service.findAll();
-			return ResponseEntity.ok().body(list);
+			List<UserDTO> listDTO = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
+			return ResponseEntity.ok().body(listDTO);
 			
 		}
 		
