153072c53e64dd76a06aa011f7454cba04c895cd
Ryan Silva <ryan.silva@showtecnologia.com>
2024-09-10 13:55:12 -0300

    Increase url search improvements, final commit

--- src/main/java/com/ifpe/project/resources/PostResource.java
+++ src/main/java/com/ifpe/project/resources/PostResource.java
@@ -1,6 +1,7 @@
 	
 package com.ifpe.project.resources;
 	
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -36,6 +37,16 @@ import com.ifpe.project.services.PostService;
 			return ResponseEntity.ok().body(list);
 		}
 
-		 
+		@RequestMapping(value="/fullsearch", method=RequestMethod.GET)
+	 	public ResponseEntity<List<Post>> fullSearch(
+	 			@RequestParam(value="text", defaultValue="") String text,
+	 			@RequestParam(value="minDate", defaultValue="") String minDate,
+	 			@RequestParam(value="maxDate", defaultValue="") String maxDate) {
+			text = URL.decodeParam(text);
+			Date min = URL.convertDate(minDate, new Date(0L));
+			Date max = URL.convertDate(maxDate, new Date());
+			List<Post> list = service.fullSearch(text, min, max);
+			return ResponseEntity.ok().body(list);
+		}
 		
 	}
